f3ea65bbdbb02918ed47bfd9fb0ccebc1e889598
venkatalakshmiTammisetti <venkatalakshmi18105@gmail.com>
2026-04-24 21:59:15 +0530

    requrements.txt

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="venkatalakshmi_tammisetti",
+    packages=find_packages(),
+    install_requires=[]
+)
